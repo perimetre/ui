05ed22bcc11d9a2061f05cea5bbef19521e9eb38
AssisrMatheus <assisr.matheus@gmail.com>
2020-11-12 15:53:44 -0300

    feat: added responsive aliases to valueType

--- src/components/ui/System/valueType.d.ts
+++ src/components/ui/System/valueType.d.ts
@@ -1,2 +1,12 @@
-type ValueType<T> = T | T[];
+type ValueType<T> =
+  | T
+  | T[]
+  | {
+      _?: T;
+      sm?: T;
+      md?: T;
+      lg?: T;
+      xl?: T;
+      xxl?: T;
+    };
 export default ValueType;
