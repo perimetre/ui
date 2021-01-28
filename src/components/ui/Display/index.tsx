0f45bf60fa4a7629681acd27f9de49700b196e02
AssisrMatheus <assisr.matheus@gmail.com>
2021-01-28 16:13:17 -0300

    refactor: deleting legacy code

--- src/components/ui/Display/index.tsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-import { boxCss } from '../System/box';
-
-const Display = styled.div`
-  ${boxCss}
-`;
-
-export default Display;
--- src/components/ui/Spacing/index.tsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-import { spacingCss } from '../System/spacing';
-
-const Spacing = styled.div`
-  ${spacingCss}
-`;
-
-export default Spacing;
