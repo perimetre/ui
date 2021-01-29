265782da62eed82cc86fdd1eb80514038726911e
AssisrMatheus <assisr.matheus@gmail.com>
2021-01-29 16:54:04 -0300

    fix: fixed css export

--- rollup.config.js
+++ rollup.config.js
@@ -9,6 +9,7 @@ import pkg from './package.json';
 
 // eslint-disable-next-line import/no-default-export
 export default [
+  // Outputs a javascript bundle for our components
   {
     input: 'src/index.tsx',
     output: [
@@ -27,13 +28,17 @@ export default [
       peerDepsExternal(),
       resolve(),
       commonjs(),
+      // Utilize a specific tsconfig only for building
       typescript({ clean: true, useTsconfigDeclarationDir: true, tsconfig: 'tsconfig.build.json' }),
-      postcss(),
+      // Modules are turned on so if a component imports the css. It wil have a unique ID so it doesn't conflict with the general css
+      postcss({ modules: true }),
+      // Copy the original postcss files so the user can choose to use those if wanted
       copy({
         targets: [{ src: 'src/styles/components*', dest: 'build/postcss' }]
       })
     ]
   },
+  // Output a general non minified index.css
   {
     input: 'src/index.css',
     output: [
@@ -42,6 +47,7 @@ export default [
         format: 'es'
       }
     ],
+    // Extract the css in a file
     plugins: [postcss({ extract: true })],
     /**
      * Removes the "overwrites a previously emitted file of the same name." warn
@@ -55,6 +61,7 @@ export default [
       warn(warning);
     }
   },
+  // Output a general minified index.min.css
   {
     input: 'src/index.css',
     output: [
@@ -63,6 +70,8 @@ export default [
         format: 'es'
       }
     ],
+    // Turn on mification
+    // Extract the css in a file
     plugins: [postcss({ minimize: true, extract: true })],
     /**
      * Removes the "overwrites a previously emitted file of the same name." warn
