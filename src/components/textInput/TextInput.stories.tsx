57782f544c82c843a91775f10ed917d4be619830
AssisrMatheus <assisr.matheus@gmail.com>
2021-02-12 11:33:45 -0300

    feat: added datalist example

--- src/components/textInput/TextInput.stories.tsx
+++ src/components/textInput/TextInput.stories.tsx
@@ -187,4 +187,53 @@ const WithButtonTemplate: Story = ({ label, error, success, loading, className, 
 
 export const WithButton = WithButtonTemplate.bind({});
 
+/**
+ * A story that displays a TextInput example
+ *
+ * @param props the story props
+ * @param props.label the label property set on controls
+ * @param props.error the error property set on controls
+ * @param props.success the success property set on controls
+ * @param props.loading the loading property set on controls
+ * @param props.className the component classes
+ */
+const DataListTemplate: Story = ({ label, error, success, loading, className, ...props }) => (
+  <div>
+    <label className="pui-label-input" htmlFor="input">
+      {label}
+    </label>
+    <span
+      className={[
+        ...(error ? ['pui-text-input-error'] : []),
+        ...(success ? ['pui-text-input-success'] : []),
+        ...(loading ? ['pui-text-input-loading'] : [])
+      ].join(' ')}
+    >
+      <input
+        {...props}
+        id="input"
+        list="listOptions"
+        name="listOptions"
+        className={[
+          `pui-text-input`,
+          // Add remaining classes
+          ...(className && className.length > 0 ? [className] : [])
+        ].join(' ')}
+      />
+      <datalist id="listOptions">
+        {Array(10)
+          .fill(null)
+          .map((_, i) => (
+            <option key={i} value={`Option ${i + 1}`}>
+              Option {i + 1}
+            </option>
+          ))}
+      </datalist>
+    </span>
+    {error && <div />}
+  </div>
+);
+
+export const DataList = DataListTemplate.bind({});
+
 // TODO: Add "With icon"
