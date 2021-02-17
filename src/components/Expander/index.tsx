52f831cad99789a10df9308b0945b82f0bd2f3e9
AssisrMatheus <assisr.matheus@gmail.com>
2021-02-17 17:01:25 -0300

    feat: added expander

---
+++ src/components/Expander/index.tsx
@@ -0,0 +1,44 @@
+import { motion } from 'framer-motion';
+import React, { useMemo, useState } from 'react';
+
+export type ExpanderProps = {
+  /**
+   * Whether or not it should be expanded
+   *
+   * @default true
+   */
+  isExpanded?: boolean;
+};
+
+/**
+ * An expander component
+ *
+ * @param props The component props
+ * @param props.isExpanded Whether or not it should be expanded
+ * @param props.children The provided children
+ */
+export const Expander: React.FC<ExpanderProps> = ({ children, isExpanded }) => {
+  // Creates the animation variants
+  const variants = useMemo(
+    () => ({
+      visible: { height: 'auto' },
+      hidden: { height: 0 }
+    }),
+    []
+  );
+
+  // Creates a state only in the first render, to get the initial variant, based on the first value of isExpanded
+  const [initialVariant] = useState(!isExpanded ? 'hidden' : 'visible');
+
+  return (
+    <motion.div
+      style={{ overflow: 'hidden' }}
+      // If it's collapsed use the hidden value, use the visible value otherwise
+      animate={!isExpanded ? 'hidden' : 'visible'}
+      initial={initialVariant}
+      variants={variants}
+    >
+      {children}
+    </motion.div>
+  );
+};
