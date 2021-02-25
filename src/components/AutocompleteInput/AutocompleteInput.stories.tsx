052070e11e9023a197162e2805feeaf00f822cda
AssisrMatheus <assisr.matheus@gmail.com>
2021-02-25 10:40:07 -0300

    refactor: moved autocomplete import

--- src/components/AutocompleteInput/AutocompleteInput.stories.tsx
+++ src/components/AutocompleteInput/AutocompleteInput.stories.tsx
@@ -1,7 +1,7 @@
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Meta, Story } from '@storybook/react/types-6-0';
 import React from 'react';
-import { AutocompleteInput, AutocompleteInputProps } from '.';
+import { AutocompleteInput, AutocompleteInputProps } from './AutocompleteInput';
 import { MenuIcon } from '../Icons';
 
 export default {
