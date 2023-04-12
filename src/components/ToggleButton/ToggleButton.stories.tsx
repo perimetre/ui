36ab4175866267b8b7132559bba0f389b6f3dd54
AssisrMatheus <assisr.matheus@gmail.com>
2023-04-12 10:40:12 -0400

    feat: adds toggle button component

--- src/components/ToggleButton/ToggleButton.stories.tsx
+++ src/components/ToggleButton/ToggleButton.stories.tsx
@@ -1,16 +1,17 @@
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Meta, Story } from '@storybook/react/types-6-0';
 import React, { useState } from 'react';
-import { ToggleInput, ToggleInputProps } from '.';
+import { ToggleButton, ToggleButtonProps } from '.';
 import { colorOptions } from '../../prebuiltTailwindTheme';
 import classnames from 'classnames';
 import { puiColorClassnameMap } from '../../storybookMappers';
 
 export default {
-  title: 'Components/Inputs/ToggleInput',
-  component: ToggleInput,
+  title: 'Components/Inputs/ToggleButton',
+  component: ToggleButton,
   argTypes: {
-    label: { defaultValue: 'Input' },
+    offLabel: { defaultValue: 'Off' },
+    onLabel: { defaultValue: 'On' },
     color: {
       defaultValue: 'pui-primary',
       control: {
@@ -21,12 +22,6 @@ export default {
     defaultChecked: {
       control: {
         type: 'boolean'
-      }
-    },
-    placeholder: {
-      defaultValue: 'Type here...',
-      control: {
-        type: 'text'
       }
     },
     disabled: {
@@ -46,16 +41,16 @@ export default {
 } as Meta;
 
 /**
- * A story that displays a ToggleInput example
+ * A story that displays a ToggleButton example
  *
  * @param props the story props
  * @param props.color User selected color
  */
-const Template: Story<ToggleInputProps & { color?: string }> = ({ color, ...props }) => {
+const Template: Story<ToggleButtonProps & { color?: string }> = ({ color, ...props }) => {
   const [checked, setChecked] = useState(false);
 
   return (
-    <ToggleInput
+    <ToggleButton
       {...props}
       className={classnames(
         { [puiColorClassnameMap[color || 'transparent']]: color !== 'pui-primary' },
@@ -70,19 +65,4 @@ const Template: Story<ToggleInputProps & { color?: string }> = ({ color, ...prop
   );
 };
 
-export const WithLabel = Template.bind({});
-
-export const NoLabel = Template.bind({});
-NoLabel.args = {
-  label: undefined
-};
-
-export const WithHelp = Template.bind({});
-WithHelp.args = {
-  help: 'You can also have a help text'
-};
-
-export const WithError = Template.bind({});
-WithError.args = {
-  error: 'Input is required'
-};
+export const Default = Template.bind({});
